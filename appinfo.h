2dd46a95dc3f5caa2d63a0dbcd3270949756038c
Konstantin Demin <rockdrilla@gmail.com>
2021-10-25 19:45:02 +0300

    bump app version

--- appinfo.h
+++ appinfo.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #define APP_TITLE    "Wintertide Brand calculator [Path of Exile 3.16]"
-#define APP_VERSION  "0.0.1"
+#define APP_VERSION  "0.0.2"
 #define APP_AUTHOR   "Konstantin Demin"
 
 #endif // APPINFO_H
