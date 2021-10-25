e33bcacb9ab77c1db62e7b219920a8fae5672435
Konstantin Demin <rockdrilla@gmail.com>
2021-10-25 19:20:41 +0300

    update numbers to 3.16 "Scourge"

--- appinfo.h
+++ appinfo.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define APP_TITLE    "Wintertide Brand calculator [Path of Exile 3.15]"
+#define APP_TITLE    "Wintertide Brand calculator [Path of Exile 3.16]"
 #define APP_VERSION  "0.0.1"
 #define APP_AUTHOR   "Konstantin Demin"
 
